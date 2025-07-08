d2a36212c5e1a3a050952ea20f38e1f5760bd297
unknown <toan9370@donga.edu.vn>
2025-07-08 16:22:13 +0700

    update type code

--- the-wild/app/about/page.tsx
+++ the-wild/app/about/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import image1 from "@/public/CV4.jpg";
 import image2 from "@/public/CV6.jpg";
-import { getCabins } from "@/app/_lib/data-cabins";
+// import { getCabins } from "@/app/_lib/data-cabins";
 import Link from "next/link";
 
 export const revalidate = 86400;
@@ -10,8 +10,9 @@ export const metadata = {
   title: "About",
 };
 
-export default async function Page() {
-  const cabins = await getCabins();
+export default function Page() {
+  // const cabins = await getCabins();
+  // console.log("cabin data: ", cabins)
 
   return (
     <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
@@ -29,7 +30,7 @@ export default async function Page() {
             and enjoying simple pleasures with family.
           </p>
           <p>
-            Our {cabins.length} luxury cabins provide a cozy base, but the real
+            {/* Our {cabins.length} luxury cabins provide a cozy base, but the real */}
             freedom and peace you&apos;ll find in the surrounding mountains.
             Wander through lush forests, breathe in the fresh air, and watch the
             stars twinkle above from the warmth of a campfire or your hot tub.
